eb2fc89506abbfc12377e9a769ef5dd5328729ef
ThalesFu <lhfu@trip.com>
2024-01-25 13:25:57 +0800

    构建objectives

---
+++ CK2/objectives/main/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"fmt"
+	"github.com/thalesfu/paradoxtools/CK2/objectives"
+	"github.com/thalesfu/paradoxtools/utils"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	//buildObjectiveField("R:\\Thales\\Game\\SteamLibrary\\steamapps\\common\\Crusader Kings II")
+
+	allObjectives := objectives.LoadAllObjective("R:\\Thales\\Game\\SteamLibrary\\steamapps\\common\\Crusader Kings II")
+
+	for _, v := range allObjectives {
+		fmt.Println(utils.MarshalJSON(v))
+	}
+}
+
+func buildObjectiveField(dir string) {
+	modifiers_dir := filepath.Join(dir, "common", "objectives")
+	// 读取目录中的所有文件和子目录
+	files, err := os.ReadDir(modifiers_dir)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	boolRegex := regexp.MustCompile(`^\t(\w+)\s*=\s(yes|no)`)
+	boolFieldMap := make(map[string]bool)
+	boolFieldsContent := ""
+	float32Regex := regexp.MustCompile(`^\t(\w+)\s*=\s-?\d+\.\d*`)
+	float32FieldMap := make(map[string]bool)
+	float32FieldsContent := ""
+	stringRegex := regexp.MustCompile(`^\t(\w+)\s*=\s([a-zA-Z]\w*)`)
+	stringFieldMap := make(map[string]bool)
+	stringFieldsContent := ""
+	intRegex := regexp.MustCompile(`^\t(\w+)\s*=\s-?\d+\.*\d*`)
+	intFieldMap := make(map[string]bool)
+	intFieldsContent := ""
+	structRegex := regexp.MustCompile(`^\t(\w+)\s*=\s\{`)
+	structFieldMap := make(map[string]bool)
+	structFieldsContent := ""
+
+	// 循环遍历文件
+	for _, file := range files {
+		if file.IsDir() {
+			continue // 跳过目录
+		}
+
+		// 获取文件名
+		filename := file.Name()
+
+		// 检查文件后缀是否为.csv或.json
+		if strings.HasSuffix(filename, ".txt") {
+			filepath := filepath.Join(modifiers_dir, filename)
+
+			content, ok := utils.LoadContent(filepath)
+
+			if ok {
+				lines := strings.Split(content, "\n")
+
+				for _, line := range lines {
+					result := boolRegex.FindStringSubmatch(line)
+
+					if len(result) > 0 {
+						w := result[1]
+
+						if boolFieldMap[w] {
+							continue
+						}
+
+						wp := strings.Split(w, "_")
+						wfn := ""
+						for _, ww := range wp {
+							wfn += cases.Title(language.English).String(ww)
+						}
+
+						boolFieldsContent += fmt.Sprintf("%s\tpserialize.PBool `paradox_field:\"%s\" json:\"%s,omitempty\"`\n", wfn, w, w)
+						boolFieldMap[w] = true
+						continue
+					}
+
+					result = stringRegex.FindStringSubmatch(line)
+
+					if len(result) > 0 {
+						w := result[1]
+
+						if stringFieldMap[w] {
+							continue
+						}
+
+						wp := strings.Split(w, "_")
+						wfn := ""
+						for _, ww := range wp {
+							wfn += cases.Title(language.English).String(ww)
+						}
+
+						stringFieldsContent += fmt.Sprintf("%s\tstring `paradox_field:\"%s\" json:\"%s,omitempty\"`\n", wfn, w, w)
+						stringFieldMap[w] = true
+						continue
+					}
+
+					result = float32Regex.FindStringSubmatch(line)
+
+					if len(result) > 0 {
+						w := result[1]
+
+						if float32FieldMap[w] {
+							continue
+						}
+
+						wp := strings.Split(w, "_")
+						wfn := ""
+						for _, ww := range wp {
+							wfn += cases.Title(language.English).String(ww)
+						}
+
+						float32FieldsContent += fmt.Sprintf("%s\tfloat32 `paradox_field:\"%s\" json:\"%s,omitempty\"`\n", wfn, w, w)
+						float32FieldMap[w] = true
+						continue
+					}
+
+					result = intRegex.FindStringSubmatch(line)
+
+					if len(result) > 0 {
+						w := result[1]
+
+						if intFieldMap[w] {
+							continue
+						}
+
+						wp := strings.Split(w, "_")
+						wfn := ""
+						for _, ww := range wp {
+							wfn += cases.Title(language.English).String(ww)
+						}
+
+						intFieldsContent += fmt.Sprintf("%s\tint `paradox_field:\"%s\" json:\"%s,omitempty\"`\n", wfn, w, w)
+						intFieldMap[w] = true
+						continue
+					}
+
+					result = structRegex.FindStringSubmatch(line)
+
+					if len(result) > 0 {
+						w := result[1]
+
+						if structFieldMap[w] {
+							continue
+						}
+
+						wp := strings.Split(w, "_")
+						wfn := ""
+						for _, ww := range wp {
+							wfn += cases.Title(language.English).String(ww)
+						}
+
+						structFieldsContent += fmt.Sprintf("%s\t*%s `paradox_field:\"%s\" json:\"%s,omitempty\"`\n", wfn, wfn, w, w)
+						structFieldMap[w] = true
+						continue
+					}
+				}
+			}
+		}
+	}
+
+	utils.WriteContent("logs/objectives_fields.txt", boolFieldsContent+stringFieldsContent+float32FieldsContent+intFieldsContent+structFieldsContent)
+}
---
+++ CK2/objectives/objective.go
@@ -0,0 +1,96 @@
+package objectives
+
+import (
+	"fmt"
+	"github.com/thalesfu/paradoxtools/CK2/localisation"
+	"github.com/thalesfu/paradoxtools/utils"
+	"github.com/thalesfu/paradoxtools/utils/pserialize"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Objective struct {
+	Code                  string           `paradox_type:"map_key" json:"code,omitempty"`
+	Name                  string           `json:"name,omitempty"`
+	Description           string           `json:"description,omitempty"`
+	CanCancel             pserialize.PBool `paradox_field:"can_cancel" json:"can_cancel,omitempty"`
+	ExpectationOfLiege    pserialize.PBool `paradox_field:"expectation_of_liege" json:"expectation_of_liege,omitempty"`
+	AiCapitalKingdomFocus pserialize.PBool `paradox_field:"ai_capital_kingdom_focus" json:"ai_capital_kingdom_focus,omitempty"`
+	Exclusive             pserialize.PBool `paradox_field:"exclusive" json:"exclusive,omitempty"`
+	RelHeadLoyalist       pserialize.PBool `paradox_field:"rel_head_loyalist" json:"rel_head_loyalist,omitempty"`
+	CancelOnLeaderDeath   pserialize.PBool `paradox_field:"cancel_on_leader_death" json:"cancel_on_leader_death,omitempty"`
+	MilitaryPlot          pserialize.PBool `paradox_field:"military_plot" json:"military_plot,omitempty"`
+	VassalRankPlot        pserialize.PBool `paradox_field:"vassal_rank_plot" json:"vassal_rank_plot,omitempty"`
+	IntriguePlot          pserialize.PBool `paradox_field:"intrigue_plot" json:"intrigue_plot,omitempty"`
+	MurderPlot            pserialize.PBool `paradox_field:"murder_plot" json:"murder_plot,omitempty"`
+	VassalIntriguePlot    pserialize.PBool `paradox_field:"vassal_intrigue_plot" json:"vassal_intrigue_plot,omitempty"`
+	Type                  string           `paradox_field:"type" json:"type,omitempty"`
+	Text                  string           `paradox_field:"text" json:"text,omitempty"`
+	Fertility             float32          `paradox_field:"fertility" json:"fertility,omitempty"`
+	Aggression            float32          `paradox_field:"aggression" json:"aggression,omitempty"`
+	GlobalRevoltRisk      float32          `paradox_field:"global_revolt_risk" json:"global_revolt_risk,omitempty"`
+	WarningLevel          float32          `paradox_field:"warning_level" json:"warning_level,omitempty"`
+	PlotPowerModifier     float32          `paradox_field:"plot_power_modifier" json:"plot_power_modifier,omitempty"`
+	Health                float32          `paradox_field:"health" json:"health,omitempty"`
+	Stewardship           int              `paradox_field:"stewardship" json:"stewardship,omitempty"`
+	TownOpinion           int              `paradox_field:"town_opinion" json:"town_opinion,omitempty"`
+	Intrigue              int              `paradox_field:"intrigue" json:"intrigue,omitempty"`
+	SexAppealOpinion      int              `paradox_field:"sex_appeal_opinion" json:"sex_appeal_opinion,omitempty"`
+	Martial               int              `paradox_field:"martial" json:"martial,omitempty"`
+	CombatRating          int              `paradox_field:"combat_rating" json:"combat_rating,omitempty"`
+	Diplomacy             int              `paradox_field:"diplomacy" json:"diplomacy,omitempty"`
+	Learning              int              `paradox_field:"learning" json:"learning,omitempty"`
+	ChurchOpinion         int              `paradox_field:"church_opinion" json:"church_opinion,omitempty"`
+}
+
+func LoadAllObjective(path string) map[string]*Objective {
+
+	translations := localisation.LoadAllTranslations(path)
+	dir := filepath.Join(path, "common", "objectives")
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return nil
+	}
+
+	result := make(map[string]*Objective)
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue // 跳过目录
+		}
+
+		// 获取文件名
+		filename := file.Name()
+
+		// 检查文件后缀是否为.csv或.json
+		if strings.HasSuffix(filename, ".txt") {
+			filepath := filepath.Join(dir, filename)
+
+			content, ok := utils.LoadContent(filepath)
+
+			if ok {
+				ts, o := pserialize.UnmarshalP[map[string]*Objective](content)
+
+				if o {
+					for k, v := range *ts {
+						result[k] = v
+					}
+				}
+			}
+		}
+	}
+
+	for _, m := range result {
+		m.Name = translations[m.Code]
+
+		if m.Name == "" {
+			m.Name = translations[m.Code+"_title"]
+		}
+
+		m.Description = translations[m.Code+"_desc"]
+	}
+
+	return result
+}
