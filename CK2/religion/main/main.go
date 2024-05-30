330acbce83eca25fc198b45e1b9eda4b8c25bf40
ThalesFu <thales@thalesfu.com>
2024-05-30 15:42:45 +0800

    创建宗教的代码文件

--- CK2/religion/main/main.go
+++ CK2/religion/main/main.go
@@ -4,11 +4,76 @@ import (
 	"fmt"
 	"github.com/thalesfu/paradoxtools/CK2/religion"
 	"github.com/thalesfu/paradoxtools/utils"
+	"html/template"
+	"os"
+	"path"
+	"strings"
 )
 
+var religionPath = "../CK2Commands/religion"
+
 func main() {
+	religionGroups := religion.LoadAllReligions("/Users/thalesfu/Windows/steam/steamapps/common/Crusader Kings II")
 
-	cultureGroups := religion.LoadAllReligions("/Users/thalesfu/Windows/steam/steamapps/common/Crusader Kings II")
+	fmt.Println(utils.MarshalJSON(religionGroups))
 
-	fmt.Println(utils.MarshalJSON(cultureGroups))
+	buildReligionFile(religionGroups)
+}
+
+func buildReligionFile(religionGroups map[string]*religion.ReligionGroup) {
+	if _, err := os.Stat(religionPath); os.IsNotExist(err) {
+		err = os.Mkdir(religionPath, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	religionTemplate, err := template.New("ReligionTemplate.txt").Funcs(template.FuncMap{"RP": replaceSpecialWords, "ES": escapeSpecialWords}).ParseFiles("Ck2/religion/main/ReligionTemplate.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.OpenFile(path.Join(religionPath, "religion.go"), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = religionTemplate.Execute(f, religionGroups)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func replaceSpecialWords(s string) string {
+	s = strings.ReplaceAll(s, " ", "_")
+	s = strings.ReplaceAll(s, "-", "_")
+	s = strings.ReplaceAll(s, "－", "_")
+	s = strings.ReplaceAll(s, "(", "")
+	s = strings.ReplaceAll(s, ")", "")
+	s = strings.ReplaceAll(s, "'", "")
+	s = strings.ReplaceAll(s, "’", "")
+	s = strings.ReplaceAll(s, ":", "")
+	s = strings.ReplaceAll(s, ".", "")
+	s = strings.ReplaceAll(s, ",", "")
+	s = strings.ReplaceAll(s, "!", "")
+	s = strings.ReplaceAll(s, "?", "")
+	s = strings.ReplaceAll(s, "“", "")
+	s = strings.ReplaceAll(s, "”", "")
+	s = strings.ReplaceAll(s, "‘", "")
+	s = strings.ReplaceAll(s, "’", "")
+	return s
+}
+
+func escapeSpecialWords(s string) string {
+	if s == "" {
+		return s
+	}
+
+	text, err := utils.DecodeEscapedText([]byte(s))
+	if err != nil {
+		fmt.Println(err)
+		return s
+	}
+	return text
 }
